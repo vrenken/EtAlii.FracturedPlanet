1c413b35256706330e1c2c4ad9f6b7f27ba2f5f4
Peter Vrenken <github.com@petervrenken.nl>
2019-11-19 21:30:24 +0100

    Added Math3d

---
+++ Source/Fractured Planet/Assets/Navigation/Scripts/Math/Math3d.Int.cs
@@ -0,0 +1,55 @@
+﻿public static partial class Math3d
+{
+    public static readonly int[] PowerOf2s =
+    {
+        1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,
+    };
+
+    public static int Abs(int n)
+    {
+        return (float)n < 0f ? -n : n;
+    }
+
+    public static int Mod(int n, int x)
+    {
+        return (n % x + x) % x;
+    }
+    
+    
+    
+    public static bool IsBetween(int value, int min, int max)
+    {
+        return value >= min && value <= max;
+    }
+
+    public static float Pow(int @base, int exponent)
+    {
+        var exponentNegative = exponent < 0;
+
+        if (exponentNegative)
+            exponent = -exponent;
+
+        var result = 1;
+
+        for (var i = 0; i < exponent; i++)
+        {
+            result *= @base;
+        }
+
+        if (exponentNegative)
+            return 1f / result;
+        else
+            return result;
+    }
+
+    public static int Clamp(int number, int min, int max)
+    {
+        if (number < min)
+            return min;
+        else if (number > max)
+            return max;
+        else
+            return number;
+    }
+
+}
---
+++ Source/Fractured Planet/Assets/Navigation/Scripts/Math/Math3d.Vector3Int.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public static partial class Math3d
+{
+    
+    public static Vector3Int Abs(Vector3Int n)
+    {
+        var x = Abs(n.x);
+        var y = Abs(n.y);
+        var z = Abs(n.z);
+
+        return new Vector3Int(x, y, z);
+    }
+
+    public static Vector3Int FloorToNearestX(Vector3Int n, int x)
+    {
+        var x1 = FloorToNearestX(n.x, x);
+        var y1 = FloorToNearestX(n.y, x);
+        var z1 = FloorToNearestX(n.z, x);
+
+        return new Vector3Int(x1, y1, z1);
+    }
+
+    public static Vector3Int Mod(Vector3Int n, int x)
+    {
+        var x1 = Mod(n.x, x);
+        var y1 = Mod(n.y, x);
+        var z1 = Mod(n.z, x);
+
+        return new Vector3Int(x1, y1, z1);
+    }
+
+}
