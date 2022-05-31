4f3fdecc3f5a348591c4efa632aed3382a5f3b17
Peter Vrenken <peter.vrenken@atos.net>
2022-06-01 00:09:22 +0200

    Finetuned area for map creation.

--- Source/Fractured Planet/Assets/Sector/Loader.cs
+++ Source/Fractured Planet/Assets/Sector/Loader.cs
@@ -5,10 +5,10 @@
     public class Loader : MonoBehaviour
     {
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             // Conduct scene loading.
-            
+
             // No need to keep the loader intact. It's a Loader...
             Destroy(gameObject);
         }
