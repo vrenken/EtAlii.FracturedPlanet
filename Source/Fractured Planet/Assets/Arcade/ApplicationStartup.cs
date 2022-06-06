2668375bc0c43fcabefd5e2e214c47f06e6becf4
Peter Vrenken <peter.vrenken@atos.net>
2022-06-06 11:55:56 +0200

    Got blurred intro screen working.

--- Source/Fractured Planet/Assets/Arcade/ApplicationStartup.cs
+++ Source/Fractured Planet/Assets/Arcade/ApplicationStartup.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using UnityEngine;
 using EtAlii.FracturedPlanet.Arcade;
 
@@ -8,21 +7,13 @@ public class ApplicationStartup : MonoBehaviour
     {
         var starter = new GameStarter();
 
-        var players = Debugger.IsAttached
-            ? new[]
-            {
-                new VisiblePlayer(WellKnownResources.Current.spawnPoints[0], PlayerType.Bot, false),
-                new VisiblePlayer(WellKnownResources.Current.spawnPoints[1], PlayerType.Bot, false),
-                new VisiblePlayer(WellKnownResources.Current.spawnPoints[2], PlayerType.Bot, false),
-                new VisiblePlayer(WellKnownResources.Current.spawnPoints[3], PlayerType.Bot, false),
-            }
-            : new[]
-            {
-                new VisiblePlayer(WellKnownResources.Current.spawnPoints[0], PlayerType.Bot, false),
-                new Player(WellKnownResources.Current.spawnPoints[1], PlayerType.Bot),
-                new Player(WellKnownResources.Current.spawnPoints[2], PlayerType.Bot),
-                new Player(WellKnownResources.Current.spawnPoints[3], PlayerType.Bot)
-            };
+        var players = new[]
+        {
+            new VisiblePlayer(WellKnownResources.Current.spawnPoints[0], PlayerType.Bot, false),
+            new Player(WellKnownResources.Current.spawnPoints[1], PlayerType.Bot),
+            new Player(WellKnownResources.Current.spawnPoints[2], PlayerType.Bot),
+            new Player(WellKnownResources.Current.spawnPoints[3], PlayerType.Bot)
+        };
         starter.Start(players);
 
         var playerScreen = Instantiate(WellKnownResources.Current.playerScreenPrefab);
