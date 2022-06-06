7c8129f720ab9ad7f2fbf1fbd9ade6a609e39565
Peter Vrenken <peter.vrenken@atos.net>
2022-06-06 12:14:04 +0200

    Added game start methods.

--- Source/Fractured Planet/Assets/Arcade/1. Player Selection Screen/PlayerScreen.cs
+++ Source/Fractured Planet/Assets/Arcade/1. Player Selection Screen/PlayerScreen.cs
@@ -2,16 +2,69 @@ using UnityEngine;
 
 namespace EtAlii.FracturedPlanet
 {
+    using EtAlii.FracturedPlanet.Arcade;
+
     public class PlayerScreen : MonoBehaviour
     {
-        private void Start()
+        public void StartOnePlayerGame()
         {
+            var starter = new GameStarter();
 
+            var players = new[]
+            {
+                new VisiblePlayer(WellKnownResources.Current.spawnPoints[0], PlayerType.Human, true),
+                new Player(WellKnownResources.Current.spawnPoints[1], PlayerType.Bot),
+                new Player(WellKnownResources.Current.spawnPoints[2], PlayerType.Bot),
+                new Player(WellKnownResources.Current.spawnPoints[3], PlayerType.Bot)
+            };
+            starter.Start(players);
+
+            Destroy(gameObject);
         }
-
-        private void Update()
+        public void StartTwoPlayerGame()
         {
+            var starter = new GameStarter();
 
+            var players = new[]
+            {
+                new VisiblePlayer(WellKnownResources.Current.spawnPoints[0], PlayerType.Human, true),
+                new VisiblePlayer(WellKnownResources.Current.spawnPoints[1], PlayerType.Human, true),
+                new Player(WellKnownResources.Current.spawnPoints[2], PlayerType.Bot),
+                new Player(WellKnownResources.Current.spawnPoints[3], PlayerType.Bot)
+            };
+            starter.Start(players);
+
+            Destroy(gameObject);
+        }
+        public void StartThreePlayerGame()
+        {
+            var starter = new GameStarter();
+
+            var players = new[]
+            {
+                new VisiblePlayer(WellKnownResources.Current.spawnPoints[0], PlayerType.Human, true),
+                new VisiblePlayer(WellKnownResources.Current.spawnPoints[1], PlayerType.Human, true),
+                new VisiblePlayer(WellKnownResources.Current.spawnPoints[2], PlayerType.Human, true),
+                new Player(WellKnownResources.Current.spawnPoints[3], PlayerType.Bot)
+            };
+            starter.Start(players);
+
+            Destroy(gameObject);
+        }
+        public void StartFourPlayerGame()
+        {
+            var starter = new GameStarter();
+
+            var players = new[]
+            {
+                new VisiblePlayer(WellKnownResources.Current.spawnPoints[0], PlayerType.Human, true),
+                new VisiblePlayer(WellKnownResources.Current.spawnPoints[1], PlayerType.Human, true),
+                new VisiblePlayer(WellKnownResources.Current.spawnPoints[2], PlayerType.Human, true),
+                new VisiblePlayer(WellKnownResources.Current.spawnPoints[3], PlayerType.Human, true)
+            };
+            starter.Start(players);
+
+            Destroy(gameObject);
         }
     }
 }
