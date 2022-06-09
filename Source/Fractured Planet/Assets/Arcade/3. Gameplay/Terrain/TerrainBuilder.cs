5ce46a0273d99cb388ecc20c6f7517aa04a582bb
Peter Vrenken <peter.vrenken@atos.net>
2022-06-09 23:34:39 +0200

    Start of TerrainBuilder.

---
+++ Source/Fractured Planet/Assets/Arcade/3. Gameplay/Terrain/TerrainBuilder.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Peter Vrenken. All rights reserved. See the license on https://github.com/vrenken/EtAlii.RemoteMesh
+
+namespace EtAlii.FracturedPlanet.Terrain
+{
+    public class TerrainBuilder
+    {
+
+    }
+}
